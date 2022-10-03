8e7a1bbf6ec911518280ae640d2e86a956677a15
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-10-03 11:00:17 -0300

    formatando os botões de prev e next

--- JS/script.js
+++ JS/script.js
@@ -6,12 +6,15 @@ const pokemonImage = document.querySelector('.pokemon_image');
 
 const form = document.querySelector('.form');
 
+const input = document.querySelector('.input_search');
+
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
-const APIResponse = await fetch((`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`))
+const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
 // console.log(APIResponse);
 
@@ -29,12 +32,15 @@ const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon);
     if (data) {
+        pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
         
         input.value = '';
-    } else {
+        searchPokemon = data.id;
+    }   else {
+        pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Not found :c';
         pokemonNumber.innerHTML = '';
     }
@@ -43,20 +49,20 @@ const renderPokemon = async (pokemon) => {
 form.addEventListener('submit', (event) => {
 
     event.preventDefault();
-
-    // console.log(input.value)
-    renderPokemon(input.value);
+    renderPokemon(input.value.toLowerCase());
     
 });
 
 buttonPrev.addEventListener('click', () => {
-    alert('prev clicked')
-    // console.log(input.value)
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
 });
 
 buttonNext.addEventListener('click', () => {
-    alert('next clicked')
-    // console.log(input.value)
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
 });
 
-renderPokemon('25');
+renderPokemon(searchPokemon);
