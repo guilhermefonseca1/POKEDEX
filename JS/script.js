de4db5993abcb54eafe7e143706d7621d6db13c3
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-10-02 21:48:08 -0300

    renderizando o número do pokemon por meio do id, usando querySelector

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,8 @@
 const pokemonName = document.querySelector('.pokemon_name');
 
+const pokemonNumber = document.querySelector('.pokemon_number');
+
+
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch((`https://pokeapi.co/api/v2/pokemon/${pokemon}`))
 
@@ -12,6 +15,7 @@ const renderPokemon = async (pokemon) => {
     
     const data = await fetchPokemon(pokemon);
     pokemonName.innerHTML = data.name;
+    pokemonName.innerHTML = data.id;
     // console.log(data);
 }
 
